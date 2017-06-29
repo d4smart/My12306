f4290a8815af1ae0d540dd000d04c16ac67a6a9c
闾晓冬 <d4smart@foxmail.com>
2017-06-29 16:01:02 +0800

    环境配置并测试通过

---
+++ src/main/java/com/d4smart/my12306/dao/UserMapper.java
@@ -0,0 +1,10 @@
+package com.d4smart.my12306.dao;
+
+import com.d4smart.my12306.pojo.User;
+
+/**
+ * Created by d4smart on 2017/6/29 14:10
+ */
+public interface UserMapper {
+    int insert(User user);
+}
---
+++ src/main/java/com/d4smart/my12306/pojo/User.java
@@ -0,0 +1,50 @@
+package com.d4smart.my12306.pojo;
+
+/**
+ * Created by d4smart on 2017/6/29 11:33
+ */
+public class User {
+
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    public User() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
