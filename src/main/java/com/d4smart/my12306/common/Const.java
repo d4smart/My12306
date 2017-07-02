67f24412940fde28e349c8268537f395111e5b3f
闾晓冬 <d4smart@foxmail.com>
2017-07-02 11:40:37 +0800

    完成后台用户管理模块

--- src/main/java/com/d4smart/my12306/common/Const.java
+++ src/main/java/com/d4smart/my12306/common/Const.java
@@ -13,6 +13,8 @@ public class Const {
 
     public static final String EMAIL = "email";
 
+    public static final String DEFAULT_PASSWORD = "654123";
+
     public interface Role {
         int ROLE_CUSTOMER = 0; // 普通用户
         int ROLE_ADMIN = 1; // 管理员
